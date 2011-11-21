e3f6ad88357b8ee94831b9624c60d90c485597b6
Lucian Ursu <lucian.ursu@gmail.com>
2011-11-21 19:32:29 +0200

    Changed field name and fixed issue with get_absolute_url

--- tasker/apps/todo/models.py
+++ tasker/apps/todo/models.py
@@ -12,11 +12,11 @@ class Task(models.Model):
     content = models.TextField()
     priority = models.PositiveSmallIntegerField(choices=PRIORITY_CHOICES)
     deadline = models.DateTimeField(null=True, blank=True)
-    active = models.BooleanField(default=True)
+    completed = models.BooleanField(default=False)
 
     def __unicode__(self):
         return u"<%s:%s>" % (self.title, self.get_priority_display())
 
     @models.permalink
     def get_absolute_url(self):
-        return ('todo:detail', self.id)
+        return ('todo:detail', str(self.id))
