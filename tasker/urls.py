41cfcce3f464da7b46e36ca5887a7a0ce9e08d95
Lucian Ursu <lucian.ursu@gmail.com>
2011-11-21 14:20:08 +0200

    Included urls from the todo app, accounts and api

--- tasker/urls.py
+++ tasker/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from tastypie.api import Api
+from todo.api import TaskResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(TaskResource())
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tasker.views.home', name='home'),
-    # url(r'^tasker/', include('tasker.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('todo.urls', namespace='todo')),
+    url(r'^accounts/', include('registration.urls')),
+    url(r'^api/', include(v1_api.urls)),
 )
