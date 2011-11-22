2063fe4175006679054af3edea77671950d6a207
Lucian Ursu <lucian.ursu@gmail.com>
2011-11-22 21:10:24 +0200

    Completed the API resource with authentication, authorization and other limitations

--- tasker/apps/todo/api.py
+++ tasker/apps/todo/api.py
@@ -1,7 +1,21 @@
 from tastypie.resources import ModelResource
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import Authorization
 from todo.models import Task
 
 class TaskResource(ModelResource):
     class Meta:
         queryset = Task.objects.all()
         resource_name = 'task'
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'post', 'put']
+        authentication = ApiKeyAuthentication()
+        authorization = Authorization()
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        return super(TaskResource, self).obj_create(
+                bundle, request, user=request.user
+        )
+
+    def apply_authorization_limits(self, request, object_list):
+        return object_list.filter(user=request.user)
