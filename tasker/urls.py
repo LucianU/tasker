1ccdb0ada8c666ffea4eb12fdc544689d0b50266
Lucian Ursu <lucian.ursu@gmail.com>
2012-10-28 14:33:39 +0200

    Installed the admin

--- tasker/urls.py
+++ tasker/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 
 from tastypie.api import Api
 from todo.api import TaskResource
+
+admin.autodiscover()
 
 v1_api = Api(api_name='v1')
 v1_api.register(TaskResource())
@@ -10,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^', include('todo.urls', namespace='todo')),
     url(r'^accounts/', include('registration.urls')),
     url(r'^api/', include(v1_api.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
